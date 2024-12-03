fc0226ca2db4818f3178ad055a89c4f8f1caf47e
cybermelon <86929210+cybermelons@users.noreply.github.com>
2024-12-03 02:53:48 -0500

    part 2

--- 3/solution.js
+++ 3/solution.js
@@ -5,10 +5,10 @@ fs.readFile("input.txt", "utf8", (err, data) => {
     console.error(err);
     return;
   }
-	main(data)
+  main(data);
 });
 
-function main(input) {
+function sumOfMults(input) {
   const mulRegex = /mul\((\d+),(\d+)\)/g;
   const mulMatches = [...input.matchAll(mulRegex)];
   const equations = mulMatches.map(([match, leftStr, rightStr]) => [
@@ -17,7 +17,38 @@ function main(input) {
   ]);
 
   const total = equations.reduce((sum, [left, right]) => sum + left * right, 0);
-
   console.log({ equations });
+  return total;
+}
+
+function main(input) {
+  const total = sumOfMults(input);
+
+  const sections = input
+    .split(/(do\(\))|(don't\(\))/)
+    .filter((d) => d !== undefined);
+
+  let isDoMode = true;
+  const sumsOfEnabled = [];
+  for (const line of sections) {
+    if (line === "don't()") {
+      isDoMode = false;
+    } else if (line === "do()") {
+      isDoMode = true;
+    } else {
+      if (isDoMode) {
+        const sum = sumOfMults(line);
+        sumsOfEnabled.push(sum);
+      }
+    }
+  }
+
+	const enabledTotal = sumsOfEnabled.reduce((sum, num) => sum+num, 0)
+
+  // Part 2: dos() and donts()
+  // split into do() and dont() sections
+  // split by dont()s. each section with a
+  // only count the do() sections
   console.log({ total });
+  console.log({ enabledTotal });
 }
