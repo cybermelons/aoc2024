140ba09addd6a10892eb57db343ed5433c9bfc8a
cybermelon <86929210+cybermelons@users.noreply.github.com>
2024-12-04 00:58:20 -0500

    cleanup

--- 4/solution.js
+++ 4/solution.js
@@ -15,96 +15,16 @@ function makeGrid(inputStr) {
   return grid;
 }
 
-function isLeftToRight(grid, x, y) {
-  try {
-    const leftToRight = [
-      grid[x][y],
-      grid[x + 1][y],
-      grid[x + 2][y],
-      grid[x + 3][y],
-    ].join("");
-    return leftToRight === "XMAS";
-  } catch (e) {
-    return false;
-  }
-}
-function isUpDown(grid, x, y) {
-  try {
-    const upDown = [
-      grid[x][y],
-      grid[x][y + 1],
-      grid[x][y + 2],
-      grid[x][y + 3],
-    ].join("");
-    return upDown === "XMAS";
-  } catch (e) {
-    return false;
-  }
-}
-
-function isDownUp(grid, x, y) {
-  try {
-    const upDown = [
-      grid[x][y],
-      grid[x][y - 1],
-      grid[x][y - 2],
-      grid[x][y - 3],
-    ].join("");
-    return upDown === "XMAS";
-  } catch (e) {
-    return false;
-  }
-}
-function isDiagRight(grid, x, y) {
-  try {
-    const word = [
-      grid[x][y],
-      grid[x + 1][y + 1],
-      grid[x + 2][y + 2],
-      grid[x + 3][y + 3],
-    ].join("");
-    return word === "XMAS";
-  } catch (e) {
-    return false;
-  }
-}
-function isDiagRight(grid, x, y) {
-  try {
-    const word = [
-      grid[x][y],
-      grid[x + 1][y + 1],
-      grid[x + 2][y + 2],
-      grid[x + 3][y + 3],
-    ].join("");
-    return word === "XMAS";
-  } catch (e) {
-    return false;
-  }
-}
-
-function isRightToLeft(grid, x, y) {
-  try {
-    const rightToLeft = [
-      grid[x][y],
-      grid[x - 1][y],
-      grid[x - 2][y],
-      grid[x - 3][y],
-    ].join("");
-    return rightToLeft === "XMAS";
-  } catch (e) {
-    return false;
-  }
-}
 function isInVector(grid, x, y, vector) {
   const [vecX, vecY] = vector;
   try {
-    const rightToLeft = [
+    const word = [
       grid[x][y],
       grid[x + vecX * 1][y + vecY * 1],
       grid[x + vecX * 2][y + vecY * 2],
       grid[x + vecX * 3][y + vecY * 3],
     ].join("");
-    return rightToLeft === "XMAS";
+    return word === "XMAS";
   } catch (e) {
     return false;
   }
@@ -153,7 +73,6 @@ function countXmasAt(grid, x, y) {
     (sum, result) => (result ? sum + 1 : sum),
     0,
   );
-  console.log({ x, y, counts });
   return counts;
 }
 
@@ -175,7 +94,6 @@ function mainCrossMas(input) {
     return isCrossmasAt(grid, x, y) ? 1 : 0;
   });
 
-  console.log({xmasCountsPerLocation })
   const crossmasCounts = xmasCountsPerLocation.reduce(
     (sum, count) => sum + count,
     0,
