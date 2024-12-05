dad0f442c1c771d8104650bdfcb616d88cb7725c
cybermelon <86929210+cybermelons@users.noreply.github.com>
2024-12-05 02:43:11 -0500

    bleh

--- 5/solution.js
+++ 5/solution.js
@@ -18,11 +18,27 @@ function main(input) {
     (sum, update) => (isUpdateValid(update, rules) ? [...sum, update] : sum),
     [],
   );
+
+  const invalidUpdates = updates.reduce(
+    (sum, update) => (!isUpdateValid(update, rules) ? [...sum, update] : sum),
+    [],
+  );
+
+  const correctedUpdates = invalidUpdates.map((update) =>
+    correctUpdate(update, rules),
+  );
+
   const middles = validUpdates.map(
     (update) => update[Math.floor(update.length / 2)],
   );
   const sum = middles.reduce((sum, n) => n + sum, 0);
+
+  const middlesCorrected = correctedUpdates.map(
+    (update) => update[Math.floor(update.length / 2)],
+  );
+  const sumInvalids = middlesCorrected.reduce((sum, n) => n + sum, 0);
   console.log({ sum });
+  console.log({ sumInvalids });
 }
 
 function makeRules(rulesStr) {
@@ -35,8 +51,34 @@ function makeRules(rulesStr) {
   return rules;
 }
 
+function correctUpdate(update, rules) {
+  for (const rule of rules) {
+    if (!rule(update)) {
+      console.log({ update, pair: rule.pair });
+
+      for (const num of rule.pair) {
+        const others = update.filter((n) => n !== num);
+
+        console.log({ update, num });
+        for (let i = 0; i < others.length; i++) {
+          const left = others.slice(0, i);
+          const right = others.slice(i, others.length);
+          const newUpdate = [...left, num, ...right];
+
+          console.log(newUpdate);
+          if (rules.every((doesPass) => doesPass(newUpdate))) {
+            return newUpdate;
+          }
+        }
+      }
+    }
+  }
+
+  throw Error("no proper order for update given rules");
+}
+
 function makeRule(rulePair) {
-  return (update) => {
+  const rule = (update) => {
     const indices = rulePair.map((num) => {
       return update.findIndex((n) => n === num);
     });
@@ -49,6 +91,9 @@ function makeRule(rulePair) {
     const [firstIdx, secondIdx] = indices;
     return firstIdx < secondIdx;
   };
+
+  rule.pair = rulePair;
+  return rule;
 }
 
 function isUpdateValid(update, rules) {
