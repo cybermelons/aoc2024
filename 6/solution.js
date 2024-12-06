2cc21f393d77bdd4b6cbc723959c7fd23de7bee7
cybermelon <86929210+cybermelons@users.noreply.github.com>
2024-12-06 03:15:05 -0500

    brute force sadly aint it

--- 6/solution.js
+++ 6/solution.js
@@ -1,6 +1,6 @@
 const fs = require("node:fs");
 
-fs.readFile("sample.txt", "utf8", (err, data) => {
+fs.readFile("input.txt", "utf8", (err, data) => {
   if (err) {
     console.error(err);
     return;
